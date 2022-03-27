29704bf0c69c077ea9d1b222620dc88766c0672c
Siavash Adib <Siyavash.Adib@gmail,com>
2022-03-27 22:20:53 +0430

    Calculator is working (All button functions except "+/-" implemented)

--- Srouce Codes/App.js
+++ Srouce Codes/App.js
@@ -1,6 +1,11 @@
 let calculator = document.querySelector('.calculator');
-let calculatorButton = [...calculator.querySelectorAll('.calculator-button')];
+let calculatorButton = [...calculator.querySelectorAll('.calculator-button:not(.disabled)')];
+let calculatorDisplay = calculator.querySelector('.calculator-display');
 
+
+let calculatorBuffer = {
+  inputString: ['0'],
+};
 
 function operate(arg1, arg2, operation) {
   switch (operation) {
@@ -15,19 +20,19 @@ function operate(arg1, arg2, operation) {
 
 function multiOperation(operationString) {
   let opStringCopy = operationString.split(/\s+/g);
-  // console.log(`1: opStringCopy : ${opStringCopy}`);
+  console.log(`1: opStringCopy : ${opStringCopy}`);
 
-  if ((/\D/).test(opStringCopy[opStringCopy.length - 1])) {
+if ((/[\+\-\*\/]/).test(opStringCopy[opStringCopy.length - 1])) {
     opStringCopy.pop();
   }
 
-  if ((/\D/).test(opStringCopy[0])) {
+if ((/[\+\-\*\/]/).test(opStringCopy[0])) {
     let firstElement = opStringCopy.shift();
     if (firstElement === '-') {
       opStringCopy[0] *= -1;
     }
   }
-  // console.log(`2: opStringCopy : ${opStringCopy}`);
+  console.log(`2: opStringCopy : ${opStringCopy}`);
 
   for (let i = 0; i < opStringCopy.length; i++) {
 
@@ -49,18 +54,94 @@ function multiOperation(operationString) {
     if ((opStringCopy[i] === '+') || (opStringCopy[i] === '-')) {
       let arg1 = opStringCopy[i - 1];
       let arg2 = opStringCopy[i + 1];
+      let operation = operate(arg1, arg2, opStringCopy[i]);
 
-      opStringCopy.splice(i - 1, 3, operate(arg1, arg2, opStringCopy[i]));
+      opStringCopy.splice(i - 1, 3, operation);
       // console.log(opStringCopy);
       i -= 1;
     }
   }
 
+  opStringCopy[0] = String(parseFloat(parseFloat(opStringCopy[0]).toFixed(4)));
   return opStringCopy[0];
 }
 
 function calculatorHandleInput(newInput) {
-  console.log(newInput);
+  // console.log(newInput);
+  let latestInput = calculatorBuffer.inputString[calculatorBuffer.inputString.length - 1];
+
+  switch (newInput) {
+    case '=':
+      console.log(calculatorBuffer.inputString);
+      calculatorBuffer.inputString = [String(multiOperation(calculatorBuffer.inputString.join(" ")))];
+      break;
+
+    case 'DEL':
+      if(isNaN(latestInput)) {
+        calculatorBuffer.inputString.pop();
+      } else {
+        latestInput = latestInput.split("").slice(0, -1).join("");
+        calculatorBuffer.inputString.pop();
+        if(String(latestInput).length !== 0) {
+          calculatorBuffer.inputString.push(String(latestInput))
+        }
+        // Everything deleted (empty input)
+        if(calculatorBuffer.inputString.length === 0) {
+          calculatorBuffer.inputString = ['0'];
+        }
+      }
+      break;
+
+    case 'C':
+    case 'CE':
+      calculatorBuffer.inputString = ['0'];
+      break;
+
+    case '.':
+      if(!latestInput.includes('.')) {
+        latestInput += newInput;
+        calculatorBuffer.inputString.pop();
+        calculatorBuffer.inputString.push(String(latestInput))
+      }
+      break;
+
+    case '+/-':
+      break;
+    
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+
+      if (isNaN(latestInput)) {
+        calculatorBuffer.inputString.pop();
+      }
+      calculatorBuffer.inputString.push(newInput);
+      break;
+
+    default:
+      if (isNaN(latestInput) && (latestInput !== '.')) {
+        calculatorBuffer.inputString.push(newInput);
+        // console.log('new number after non number');
+      } else {
+        // console.log('new number after number');
+        // console.log(`latestInput = ${latestInput}`);
+
+        if(latestInput === '0') {
+          latestInput = newInput;
+        } else {
+          latestInput += newInput;
+        }
+        // console.log(`latestInput = ${latestInput}`);
+
+        calculatorBuffer.inputString.pop();
+        calculatorBuffer.inputString.push(String(latestInput))
+      }
+      break;
+  }
+
+  console.table(calculatorBuffer);
+  calculatorUpdateDisplay();
 }
 
 function calculatorButtonEvent() {
@@ -73,19 +154,20 @@ function userKeyboardEvent(e) {
   switch (e.key) {
     case 'Delete':
     case 'Backspace': userKeyboardInput = 'DEL'; break;
-    
+
     case 'm':
     case 'M': userKeyboardInput = '+/-'; break;
-    
+
+    case 'Escape':
     case 'c':
     case 'C': userKeyboardInput = 'C'; break;
-    
+
     case 'Enter': userKeyboardInput = '='; break;
 
     default: break;
   }
   // console.log(userKeyboardInput);
-  
+
   const ACCEPTABLE_INPUTS_REGEXP = (
     /^([CM0-9\=\.\+\-\*\/]|DEL|CE)$/
   );
@@ -93,7 +175,7 @@ function userKeyboardEvent(e) {
 
     correspondingButton = calculatorButton.find((button) => button.innerText === userKeyboardInput);
 
-    if(e.type === 'keydown') {
+    if (e.type === 'keydown') {
       calculatorHandleInput(userKeyboardInput);
       correspondingButton.classList.add('button-pressed');
     }
@@ -103,8 +185,15 @@ function userKeyboardEvent(e) {
         button.classList.remove('button-pressed');
       });
     }, 100);
-    
   }
+}
+
+function calculatorUpdateDisplay() {
+  let displayString = [...calculatorBuffer.inputString];
+  calculatorDisplay.innerText =
+  displayString
+    .map((element) => element.match(/^[\+\-\*\/]$/g) ? ` ${element} ` : element)
+    .join("");
 }
 
 
